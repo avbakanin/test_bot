c83e95e7f4e685a273de139836e64381c1601b2d
Alex <avbakanin@gmail.com>
2022-04-25 19:46:43 +0300

    class Db_connected_funcs was realised

--- main.py
+++ main.py
@@ -16,13 +16,11 @@ def start_message(message):
     us_name = message.from_user.username
     log_in_date = datetime.datetime.now()
 
-    bot.send_message(message.from_user.id, "Привет, " + us_first_name +
-                            '!\n\n'
-                            + "Я - бот, который поможет узнать название региона "
-                              "по его коду c авто номера.\n"
-                              "\n"
-                              "Выберите действие:",
-                            reply_markup=ui_functions.create_buttons())
+    bot.send_message(message.from_user.id,
+                     "Привет, %s!\n\n"
+                     "Я - бот, который поможет узнать название региона по его коду c авто номера.\n\n"
+                     "Выберите действие:" % us_first_name,
+                     reply_markup=ui_functions.create_buttons())
 
     print(tg_us_id, us_first_name, us_last_name, us_name, log_in_date)
 
@@ -33,24 +31,24 @@ def start_message(message):
 def search_code(message):
     if message.text != 'Ввести код региона:':
         bot.send_message(message.from_user.id,
-                         "Прежде чем отправить код региона \n"
+                         "Прежде чем отправить код региона,\n"
                          "нажмите на кнопку 'Ввести код региона' ----> [: :]")
     elif message.text == 'Ввести код региона:':
         bot.register_next_step_handler(message, get_code)
+
 
 def get_code(message):
     get_value = db_functions.check_code(message.text)
 
     if get_value:
-        info = (str(get_value[0]) + "\n \nЕсли хотите узнать еще один регион, "
-                                    "нажмите на кнопку \n'Ввести код региона' повторно "
-                                    "и введите код")
+        info = ("%s\n\nЕсли хотите узнать еще один регион, "
+                "нажмите на кнопку \n'Ввести код региона' "
+                "повторно и введите код" % str(get_value[0]))
         bot.send_message(message.from_user.id, info)
     else:
-         bot.send_message(message.from_user.id, "Вы ввели несуществующий код" +
-                             "\n \nЕсли хотите узнать регион, "
-                             "нажмите на кнопку \n'Ввести код региона' повторно "
-                             "и введите код")
+        bot.send_message(message.from_user.id,
+                         "Вы ввели несуществующий код.\n\nЕсли хотите узнать регион, "
+                         "нажмите на кнопку\nВвести код региона' повторно и введите код")
 
     # elif message.text == 'Вывести весь список':
 
